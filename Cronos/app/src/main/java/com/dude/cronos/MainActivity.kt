b05c743adb9a32ba6f019cc54e2ac3ced720e1cd
Christian <Timmi0701@googlemail.com>
2019-12-04 18:19:10 +0100

    Readme.md hinzugefügt

---
+++ Cronos/app/src/main/java/com/dude/cronos/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.dude.cronos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val appID:String="APP_ID"
+
+        CometChat.init(this,appID, object : CometChat.CallbackListener<String>() {
+            override fun onSuccess(p0: String?) {
+                Log.d(TAG, "Initialization completed successfully")
+            }
+
+            override fun onError(p0: CometChatException?) {
+                Log.d(TAG, "Initialization failed with exception: " + p0?.message)
+            }
+        })
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Chat
+
+Hier sind 2 Chat Projekte
+Eine ("Cronos") davon ist nicht vollständig.
